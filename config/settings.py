742a9e09d99c85dbe5268b067b24191530a94263
thesooge <thesooge.8313@gmail.com>
2024-09-09 19:02:06 +0330

    add new db

--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
     }
 }
 
