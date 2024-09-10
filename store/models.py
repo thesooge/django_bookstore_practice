e7473add9c7e6115f8fcf7f21d2c734b90c3319e
thesooge <thesooge.8313@gmail.com>
2024-09-10 17:49:26 +0330

    add order and orderitems models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -13,9 +14,34 @@ class CartItem(models.Model):
     book = models.ForeignKey(BookModel, on_delete=models.CASCADE)
     qauntity = models.PositiveSmallIntegerField()
     price = models.PositiveIntegerField()
-    total_price = models.CharField(max_length=255)
+    total_price = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.book.title}"
 
 
+class Order(models.Model):
+    ORDER_STATUS = [
+        ('up','UnPaid'),
+        ('p','Paid'),
+    ]
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='order_detail')
+    status = models.CharField(max_length=255, choices=ORDER_STATUS)
+
+    order_datetime_created = models.DateTimeField(auto_now_add=True)
+    order_datetime_modified = models.DateTimeField(auto_now=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='order_items')
+    book = models.ForeignKey(BookModel, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    price = models.IntegerField()
+    total_price = models.FloatField(null=True, blank=True)
+
+    def save(self):
+        self.total_price = self.quantity * self.price
+
+        super(OrderItem, self).save(*args, **kwargs)
+     
+        
